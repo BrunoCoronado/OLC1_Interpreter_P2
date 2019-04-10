4bfa3bb6ca39d7ad3db631d613c505c6abcd394b
Bruno Coronado <bcoronado.morales@gmail.com>
2019-04-09 20:43:43 -0600

    Agregar archivos de proyecto.

---
+++ OLC1_Interpreter_P2/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OLC1_Interpreter_P2
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
