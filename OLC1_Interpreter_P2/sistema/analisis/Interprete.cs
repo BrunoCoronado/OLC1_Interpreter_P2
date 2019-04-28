e68002dc9118dc2396081094f718a91437e22105
Bruno Coronado <bcoronado.morales@gmail.com>
2019-04-27 19:33:11 -0600

    Análisis inicial para una clase con imports

--- OLC1_Interpreter_P2/sistema/analisis/Interprete.cs
+++ OLC1_Interpreter_P2/sistema/analisis/Interprete.cs
@@ -1,6 +1,8 @@
 ﻿using Irony.Parsing;
+using OLC1_Interpreter_P2.sistema.bean;
 using OLC1_Interpreter_P2.sistema.graficador;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,15 @@ namespace OLC1_Interpreter_P2.sistema.analisis
 {
     class Interprete : Grammar
     {
+        private ParseTreeNode raiz;
+        private ArrayList clases;
+
+        public Interprete()
+        {
+            raiz = null;
+            clases = new ArrayList();
+        }
+
         public bool analizar(String contenido)
         {
             Gramatica gramatica = new Gramatica();
@@ -21,9 +32,52 @@ namespace OLC1_Interpreter_P2.sistema.analisis
             {
                 Grafica grafica = new Grafica();
                 grafica.graficar(parseTree.Root);
+                raiz = parseTree.Root;
+                analizarAST();
+                foreach (Clase clase in clases)
+                {
+                    Console.WriteLine(clase.identificador);
+                    if (clase.imports.Count > 0)
+                    {
+                        foreach (String import in clase.imports)
+                        {
+                            Console.WriteLine("\t"+import);
+                        }
+                    }
+                }
                 return true;
             }
             return false;
+        }
+
+        private void analizarAST()
+        {
+            if (raiz != null)
+                recorrerAST();
+        }
+
+        private void recorrerAST()
+        {
+            foreach (ParseTreeNode nodo in raiz.ChildNodes)
+            {
+                recorrerDeclaracionClase(nodo);
+            }
+        }
+
+        private void recorrerDeclaracionClase(ParseTreeNode declaracionClase)
+        {
+            //lecutar identificador de la clase
+            Clase clase = new Clase(declaracionClase.ChildNodes.ElementAt(0).ToString().Replace("(identificador)", "").Trim());
+            clases.Add(clase);
+            //lectura de import o sentencias internas de la clase segun sea el caso
+            if (declaracionClase.ChildNodes.Count > 1)
+            {
+                if (declaracionClase.ChildNodes.ElementAt(1).ToString().Equals("IMPORTAR"))
+                {
+                    foreach (ParseTreeNode nodo in declaracionClase.ChildNodes.ElementAt(1).ChildNodes)
+                        clase.agregarImport(nodo.ToString().Replace("(identificador)", "").Trim());
+                }
+            }
         }
     }
 }
--- OLC1_Interpreter_P2/sistema/bean/Clase.cs
+++ OLC1_Interpreter_P2/sistema/bean/Clase.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,21 @@ namespace OLC1_Interpreter_P2.sistema.bean
 {
     class Clase
     {
-        private string identificador;
+        private string _identificador;
+        private ArrayList _imports;
+
+        public Clase(String identificador)
+        {
+            _identificador = identificador;
+            _imports = new ArrayList();
+        }
+
+        public void agregarImport(String clase)
+        {
+            _imports.Add(clase);
+        }
+
+        public string identificador { get => _identificador; set => _identificador = value; }
+        public ArrayList imports { get => _imports; }
     }
 }
